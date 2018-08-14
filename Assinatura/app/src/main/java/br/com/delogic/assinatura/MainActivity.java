1f26187ebda9a678a7650537fc5dc08f1ab38a79
rodrigo <rodrigo@delogic.com.br>
2018-08-13 23:29:55 -0300

    ajuste

---
+++ Assinatura/app/src/main/java/br/com/delogic/assinatura/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.delogic.assinatura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clear(View view){
+        SignatureView signatureView = (SignatureView) findViewById(R.id.signatureView);
+        signatureView.clear();
+    }
+
+}
